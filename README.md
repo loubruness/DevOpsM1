b2c9ab9234e2c1c1d29a197e576cd8f4967c4d36
Hugo PANEL <hugopanel@gmail.com>
2024-04-26 17:27:47 +0200

    Vagrantfile to install and setup GitLab.

---
+++ README.md
@@ -0,0 +1,5 @@
+> Warning: This branch is for Apple Silicon (ARM64) machines.
+
+---
+
+The initial root password in GitLab is `076R#kN9N2-;`.
---
+++ setup-gitlab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get update -qq
+sudo apt-get install -qq -y vim git wget curl
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+
+sudo apt-get update -qq
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+sudo apt install -y gitlab-ce
+sudo gitlab-ctl reconfigure
+
+# Set initial root password
+sudo gitlab-rails runner "user = User.where(id: 1).first; user.password = '076R#kN9N2-;'; user.password_confirmation = '076R#kN9N2-;'; user.save!"

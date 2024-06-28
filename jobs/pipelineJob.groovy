943ae34b30fa502108e0a14e91fc768704068b8c
Hugo PANEL <hugopanel@gmail.com>
2024-06-28 18:32:08 +0200

    Push vers le registry qui fonctionne

--- jobs/pipelineJob.groovy
+++ jobs/pipelineJob.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     environment {
-        DOCKER_REGISTRY = 'localhost:5000' // Change this to your private registry's address
+        DOCKER_REGISTRY = 'localhost:5001' // Change this to your private registry's address
         IMAGE_NAME = 'my-project-image' // Change this to your desired image name
     }
 
@@ -24,9 +24,13 @@ pipeline {
             steps {
                 script {
                     // Log in to your Docker registry
-                    docker.withRegistry("http://${DOCKER_REGISTRY}", 'registry-credentials-id') {
+                    docker.withRegistry("http://${DOCKER_REGISTRY}") {
                         // Push the image to your private registry
-                        docker.image("${DOCKER_REGISTRY}/${IMAGE_NAME}").push()
+                        docker.image("eshopwebmvc").push()
+                    }
+                    docker.withRegistry("http://${DOCKER_REGISTRY}") {
+                        // Push the image to your private registry
+                        docker.image("eshoppublicapi").push()
                     }
                 }
             }

d2f5e4681913f7140deabbfc7cbdb1e65e860435
Hugo PANEL <hugopanel@gmail.com>
2024-06-28 18:08:29 +0200

    Essai de build sur l'agent avec docker

--- jobs/pipelineJob.groovy
+++ jobs/pipelineJob.groovy
@@ -1,55 +1,38 @@
 pipeline {
-    agent {
-        node {
-            label 'agent'
-        }
-    }
+    agent any
 
     environment {
-        DOCKER_HOST = 'tcp://docker:2376'
-        DOCKER_CERT_PATH = '/certs/client'
-        DOCKER_TLS_VERIFY = '1'
+        DOCKER_REGISTRY = 'localhost:5000' // Change this to your private registry's address
+        IMAGE_NAME = 'my-project-image' // Change this to your desired image name
     }
 
     stages {
         stage('Checkout') {
             steps {
-                checkout scm
+                // Checks out the project source code
+                git url: 'https://github.com/dotnet-architecture/eShopOnWeb.git', branch: 'main'
             }
         }
 
-        stage('Build') {
+        stage('Build Docker Image') {
             steps {
                 script {
-                    docker.build('my-app')
+                    // Builds the Docker image using the Dockerfile in the project root
+                    docker.build("${DOCKER_REGISTRY}/${IMAGE_NAME}")
                 }
             }
         }
 
-        stage('Test') {
+        stage('Push Image to Registry') {
             steps {
                 script {
-                    docker.image('my-app').inside {
-                        sh 'make test'
+                    // Log in to your Docker registry
+                    docker.withRegistry("http://${DOCKER_REGISTRY}", 'registry-credentials-id') {
+                        // Push the image to your private registry
+                        docker.image("${DOCKER_REGISTRY}/${IMAGE_NAME}").push()
                     }
                 }
             }
-        }
-
-        stage('Deploy') {
-            steps {
-                script {
-                    docker.image('my-app').inside {
-                        sh 'make deploy'
-                    }
-                }
-            }
-        }
-    }
-
-    post {
-        always {
-            cleanWs()
         }
     }
 }

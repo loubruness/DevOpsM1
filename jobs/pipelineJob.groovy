41909c8ce7207a858ee12f6af0e0ad83229958f2
ArthB94 <80051198+ArthB94@users.noreply.github.com>
2024-07-08 17:44:07 +0200

    init

--- jobs/pipelineJob.groovy
+++ jobs/pipelineJob.groovy
@@ -6,75 +6,102 @@ pipeline {
     }
 
     environment {
-        DOCKER_REGISTRY = 'localhost:5001' // Change this to your private registry's address
-        IMAGE_NAME = 'my-project-image' // Change this to your desired image name
+        DOCKER_REGISTRY = 'localhost:5001'
+        DOCKER_PASSWORD = '$Pa82!Da98' // Escaping special characters with single quotes
     }
 
     stages {
         stage('Prepare Environment') {
             steps {
                 script {
-                    // Adjust the permissions of the Docker socket
                     sh 'sudo chmod 666 /var/run/docker.sock'
-                    slackSend color: 'good', message: "Pipeline started: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) on agent '${env.NODE_NAME}'"
+                    echo 'Environment prepared'
                 }
             }
         }
 
         stage('Checkout') {
             steps {
-                // Checks out the project source code
                 git url: 'https://github.com/dotnet-architecture/eShopOnWeb.git', branch: 'main'
-                script{
-                    slackSend color: 'good', message: "Checkout completed"
-                }
+                echo 'Code checked out'
             }
         }
 
         stage('Build Docker Image') {
             steps {
                 sh 'docker compose build'
-                script{
-                    slackSend color: 'good', message: "Docker image built"
-                }
+                echo 'Docker images built'
             }
         }
 
         stage('Push Image to Registry') {
             steps {
                 script {
-                    // Log in to your Docker registry
                     docker.withRegistry("http://${DOCKER_REGISTRY}") {
                         // Push the image to your private registry
                         sh 'docker tag localhost:5001eshopwebmvc localhost:5001/eshopwebmvc:latest'
                         sh 'docker push localhost:5001/eshopwebmvc:latest'
                     }
-                    slackSend color: 'good', message: "Image pushed to registry"
+                    // slackSend color: 'good', message: "Image pushed to registry"
 
                     docker.withRegistry("http://${DOCKER_REGISTRY}") {
                         // Push the image to your private registry
                         sh 'docker tag localhost:5001eshoppublicapi localhost:5001/eshoppublicapi:latest'
                         sh 'docker push localhost:5001/eshoppublicapi:latest'
                     }
+                }
+            }
+        }
 
-                    slackSend color: 'good', message: "Image pushed to registry"
+        stage('Start Final Server') {
+            steps {
+                script {
+                    def finalServerStatus = sh(script: "docker inspect -f '{{.State.Running}}' final-server", returnStdout: true).trim()
+                    if (finalServerStatus != 'true') {
+                        sh 'docker compose up -d final-server'
+                        echo 'Final server started'
+                    } else {
+                        echo 'Final server is already running'
+                    }
+                }
+            }
+        }
+
+        stage('Deploy to Final Server') {
+            steps {
+                script {
+
+                    // Copy the Docker Compose file to final-server
+                    //sh 'docker cp docker-compose.yml final-server:/var/www/html/docker-compose.yml'
+                    
+
+                    sh 'docker exec final-server docker login -u paulinedav -p \'"$DOCKER_PASSWORD"\' http://localhost:5001'
+
+                    // Using single quotes around password to handle special characters
+                    docker.withRegistry("http://${DOCKER_REGISTRY}") {
+                        sh '''
+                        docker exec final-server /bin/bash -c "
+                        docker pull localhost:5001/eshoppublicapi:latest
+                        docker pull localhost:5001/eshopwebmvc:latest
+                        cat /var/www/html/docker-compose.yml
+                        docker compose -f /var/www/html/docker-compose.yml up -d
+                        "
+                        '''
+                    }
+                    echo 'App deployed to final-server'
                 }
             }
         }
     }
-    
-    post { // 
+
+    post {
         always {
             script {
                 currentBuild.result = currentBuild.result ?: 'SUCCESS'
-                
-                // Envoi de la notification Slack
-                slackSend color: currentBuild.result == 'SUCCESS' ? 'good' : 'danger', message: "Pipeline ${currentBuild.result}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) on agent '${env.NODE_NAME}'"
-                
-                // Autres actions à effectuer après l'exécution de la pipeline
-                echo 'Pipeline finished!'
+                echo "Pipeline ${currentBuild.result}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) on agent '${env.NODE_NAME}'"
             }
         }
     }
 }
+
 
--- pipeline.groovy
+++ pipeline.groovy
@@ -70,20 +70,24 @@ pipeline {
         stage('Deploy to Final Server') {
             steps {
                 script {
+
                     // Copy the Docker Compose file to final-server
-                    sh 'docker cp docker-compose.yml final-server:/var/www/html/docker-compose.yml'
+                    //sh 'docker cp docker-compose.yml final-server:/var/www/html/docker-compose.yml'
                     
-                    // Using single quotes around password to handle special characters
+
                     sh 'docker exec final-server docker login -u paulinedav -p \'"$DOCKER_PASSWORD"\' http://localhost:5001'
 
-                    // Run Docker Compose commands inside final-server
-                    sh '''
-                    docker exec final-server /bin/bash -c "
-                    docker pull localhost:5001/eshoppublicapi:latest
-                    docker pull localhost:5001/eshopwebmvc:latest
-                    docker compose -f /var/www/html/docker-compose.yml up -d
-                    "
-                    '''
+                    // Using single quotes around password to handle special characters
+                    docker.withRegistry("http://${DOCKER_REGISTRY}") {
+                        sh '''
+                        docker exec final-server /bin/bash -c "
+                        docker pull localhost:5001/eshoppublicapi:latest
+                        docker pull localhost:5001/eshopwebmvc:latest
+                        cat /var/www/html/docker-compose.yml
+                        docker compose -f /var/www/html/docker-compose.yml up -d
+                        "
+                        '''
+                    }
                     echo 'App deployed to final-server'
                 }
             }
@@ -101,95 +105,3 @@ pipeline {
 }
 
 
-
-
-// pipeline {
-//     agent {
-//         node {
-//             label 'agent'
-//         }
-//     }
-
-//     environment {
-//         DOCKER_REGISTRY = 'localhost:5001'
-//         IMAGE_NAME = 'my-project-image'
-//     }
-
-//     stages {
-//         stage('Prepare Environment') {
-//             steps {
-//                 script {
-//                     sh 'sudo chmod 666 /var/run/docker.sock'
-//                     //slackSend color: 'good', message: "Pipeline started: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) on agent '${env.NODE_NAME}'"
-//                 }
-//             }
-//         }
-
-//         stage('Checkout') {
-//             steps {
-//                 git url: 'https://github.com/dotnet-architecture/eShopOnWeb.git', branch: 'main'
-//                 // script {
-//                 //     //slackSend color: 'good', message: "Checkout completed"
-//                 // }
-//             }
-//         }
-
-//         stage('Build Docker Image') {
-//             steps {
-//                 sh 'docker compose build'
-//                 // script {
-//                 //     slackSend color: 'good', message: "Docker image built"
-//                 // }
-//             }
-//         }
-
-//         stage('Push Image to Registry') {
-//             steps {
-//                 script {
-//                     docker.withRegistry("http://${DOCKER_REGISTRY}") {
-//                         sh 'docker tag localhost:5001/eshopwebmvc localhost:5001/eshopwebmvc:latest'
-//                         sh 'docker push localhost:5001/eshopwebmvc:latest'
-//                     }
-//                     //slackSend color: 'good', message: "eshopwebmvc Image pushed to registry"
-
-//                     docker.withRegistry("http://${DOCKER_REGISTRY}") {
-//                         sh 'docker tag localhost:5001/eshoppublicapi localhost:5001/eshoppublicapi:latest'
-//                         sh 'docker push localhost:5001/eshoppublicapi:latest'
-//                     }
-//                     //slackSend color: 'good', message: "eshoppublicapi Image pushed to registry"
-//                 }
-//             }
-//         }
-
-//         stage('Start Final Server') {
-//             steps {
-//                 script {
-//                     def finalServerStatus = sh(script: "docker inspect -f '{{.State.Running}}' final-server", returnStdout: true).trim()
-//                     if (finalServerStatus != 'true') {
-//                         sh 'docker compose up -d final-server'
-//                     }
-//                 }
-//             }
-//         }
-
-//         stage('Deploy to Final Server') {
-//             steps {
-//                 script {
-//                     sh 'docker compose up -d eshopwebmvc eshoppublicapi'
-//                     //slackSend color: 'good', message: "Deployment to final-server completed"
-//                     echo 'App deployed'
-//                 }
-//             }
-//         }
-//     }
-
-//     post {
-//         always {
-//             script {
-//                 currentBuild.result = currentBuild.result ?: 'SUCCESS'
-//                 //slackSend color: currentBuild.result == 'SUCCESS' ? 'good' : 'danger', message: "Pipeline ${currentBuild.result}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL}) on agent '${env.NODE_NAME}'"
-//                 echo 'Pipeline finished!'
-//             }
-//         }
-//     }
-// }
